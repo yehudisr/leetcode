18bc1b455ea737926521df599db945044d109553
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-07-18 16:10:11 -0400

    solution

--- 7.js
+++ 7.js
@@ -2,3 +2,17 @@
 // If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
 // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Example 1:
+// Input: x = 123
+// Output: 321
+
+// Example 2:
+// Input: x = -123
+// Output: -321
+
+var reverse = function(x) {
+    const absReversed = Math.abs(x).toString().split('').reverse().join('');
+    if (absReversed > 2**31) return 0;
+    return absReversed * Math.sign(x);
+};
