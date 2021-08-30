bfe8cf73e5a923d983b0691266e941bd0bef10bd
Yehudis <yehudisr@gmail.com>
2021-08-30 13:21:55 -0400

    another solution

--- 22.js
+++ 22.js
@@ -5,20 +5,34 @@
 // for any number is divisible by 5, return buzz
 // for any number divisible by 3 and 5, return fizz buzz
 
-arr = [1, ... 100]
+// arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-function fizzbuzz(num){
+// function fizzbuzz(num){
 
-    if (num % 3 === 0) {
-        return "fizz"
-    } else if(num % 5 === 0){
-        return "buzz"
-    } else if (num % 3 === 0 && num % 5 === 0){
-        return "fizzbuzz"
-    } else {
-        return num
+//     if (num % 3 === 0) {
+//         return "fizz"
+//     } else if(num % 5 === 0){
+//         return "buzz"
+//     } else if (num % 3 === 0 && num % 5 === 0){
+//         return "fizzbuzz"
+//     } else {
+//         return num
+//     }
+
+// }
+
+// arr.map(fizzbuzz)
+
+
+for(let i = 1; i <=100; i++){
+
+    if (i % 15 === 0){
+        console.log("fizzbuzz")
+    } else if ( i % 3 === 0){
+        console.log("fizz")
+    } else if ( i % 5 === 0){
+        console.log("buzz")
+    } else{
+        console.log(i)
     }
-
 }
-
-arr.map(fizzbuzz)
